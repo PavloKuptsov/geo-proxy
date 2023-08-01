63dc5ce03b5b2c59be9690f49f6fc26516abe4ab
Oleksii Sirenko <sirenko.alexey@gmail.com>
2023-08-01 15:53:03 +0300

    Fix system.py 4

--- src/system.py
+++ src/system.py
@@ -3,17 +3,17 @@ import traceback
 
 KRAKEN_POWER_RELAY_PIN_BCM = 27
 
+
 def turn_kraken_off():
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
+    GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.LOW)
+
+
+def turn_kraken_on():
     GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
     GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.HIGH)
 
-def turn_kraken_on():
-    try:
-        GPIO.cleanup()
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
-        GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.LOW)
-    except:
-        print(traceback.format_exc())
