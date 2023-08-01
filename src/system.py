8ec73bdced7cb87a5416f5731c6c1105c6e2042b
Oleksii Sirenko <sirenko.alexey@gmail.com>
2023-08-01 15:34:39 +0300

    Fix system.py

--- src/system.py
+++ src/system.py
@@ -3,11 +3,13 @@ import RPi.GPIO as GPIO
 KRAKEN_POWER_RELAY_PIN_BCM = 27
 
 def turn_kraken_off():
+    GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
     GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.HIGH)
 
 def turn_kraken_on():
+    GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
     GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.LOW)
