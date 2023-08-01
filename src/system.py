c87654db7800e4f28ae7a70ca0c02de675253875
Oleksii Sirenko <sirenko.alexey@gmail.com>
2023-08-01 15:31:58 +0300

    Add system.py

---
+++ src/system.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+
+KRAKEN_POWER_RELAY_PIN_BCM = 27
+
+def turn_kraken_off():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
+    GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.HIGH)
+
+def turn_kraken_on():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.OUT)
+    GPIO.output(KRAKEN_POWER_RELAY_PIN_BCM, GPIO.LOW)
