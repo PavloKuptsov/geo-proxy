4ae84e59791b4b4943f287092c041a2240b5e981
Pavlo Kuptsov <pavlo.kupstov@sigma.software>
2024-05-07 23:39:09 +0300

    Improved install script

--- install.sh
+++ install.sh
@@ -2,43 +2,53 @@
 
 install_dependencies()
 {
-  echo "Installing dependencies"
-#  apt update
+  echo "[-    ] Installing dependencies"
   apt install python3-pip
-  pip install --system -r requirements.txt
+  echo "[-    ] Creating virtualenv"
+  python3 -m virtualenv venv
+  echo "[+    ] Virtualenv created"
+  echo "[+-   ] Installing python packages"
+  venv/bin/pip install -r requirements.txt
+  echo "[++   ] Packages installed"
 }
 
 remove_old_service()
 {
-    echo "Checking for the old service"
-    if [ "$(systemctl is-active gunicorn)" = "active" ];
-    then
-      echo "Old service version found, removing"
-      systemctl stop gunicorn
-      systemctl disable gunicorn
-      rm /etc/systemd/system/gunicorn.service
-    fi
+  if [ "$(systemctl is-active gunicorn)" = "active" ];
+  then
+    systemctl stop gunicorn
+    systemctl disable gunicorn
+    rm /etc/systemd/system/gunicorn.service
+  fi
 }
 
 install_service()
 {
-    echo "Installing the service"
-    if [ "$(systemctl is-active sunflower)" = "inactive" ];
-    then
-      cp sunflower.service /etc/systemd/system/
-      systemctl daemon-reload
-      systemctl enable sunflower
-      systemctl start sunflower
-    else
-      systemctl restart sunflower
-    fi
+  echo "[+++- ] Installing the service"
+  if [ "$(systemctl is-active sunflower)" = "inactive" ];
+  then
+    cp sunflower.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable sunflower
+    systemctl start sunflower
+    echo "[++++ ] Service installed successfully"
+  else
+    systemctl restart sunflower
+    echo "[++++ ] Service updated successfully"
+  fi
 }
 
 main()
 {
-  install_dependencies
+  if install_dependencies; then
+    echo "[+++  ] Dependencies installed successfully"
+  else
+    echo "[!!!  ] Error during dependency installation"
+    exit 1
+  fi
   remove_old_service
   install_service
+  echo "[+++++] Installation complete"
 }
 
 main "$@"
