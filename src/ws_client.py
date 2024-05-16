64dcaad5f36309ae273d86cff9f7e69d4f907004
Pavlo Kuptsov <pavlo.kupstov@sigma.software>
2024-05-16 17:04:12 +0300

    Fixed endless recursion in ws_client

--- src/ws_client.py
+++ src/ws_client.py
@@ -4,16 +4,22 @@ import websocket
 
 WEB_UI_WS_URL = 'ws://127.0.0.1:8080/_push'
 # WEB_UI_WS_URL = 'ws://100.96.1.2:8080/_push'
+RESTART_ATTEMPTS = 5
 
 
 class ClientSocket:
     def __init__(self, url):
         self.ws = websocket.WebSocketApp(url, on_close=self.on_close)
+        self.restarts = 0
         self.ws.run_forever()
 
     def on_close(self, ws, close_status_code, close_msg):
-        print('Connection closed, reinstating')
-        self.ws.run_forever()
+        if self.restarts <= RESTART_ATTEMPTS:
+            print('Connection closed, reinstating')
+            self.restarts += 1
+            self.ws.run_forever()
+        else:
+            print('Connection closed, restart attempts exceeded')
 
 
 def run():
